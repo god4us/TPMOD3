839807d33a19f05ed3829d70b86d84e93b7244fb
rifkyfrds <162055279+god4us@users.noreply.github.com>
2024-03-04 03:07:50 +0700

    Commit akhir menambahkan fitur gui

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text; // Fetch text from textBox1
+            if (inputText.ToUpper() == "NAMA_PRAKTIKAN")
+            {
+                label1.Text = "Halo NAMA_PRAKTIKAN"; // Assign text to label1's Text property
+            }
+            else
+            {
+                label1.Text = "HALO " + inputText; // Assign text to label1's Text property
+            }
+        }
+    }
+}
